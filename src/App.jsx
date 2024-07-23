6732f58f5fd96e58b326df0bffe5455900f87685
maydindev <maydin.dev@outlook.com>
2024-07-23 11:13:03 +0300

    Added Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import housesForSale from "./data/housesForSale";
+import HouseCard from "./components/HouseCard";
 
 export default function App() {
   /* Challenge
@@ -21,64 +22,7 @@ export default function App() {
 
   const houseCards = housesForSale.map((houseData, index, array) => {
     return (
-      <div className="house-card" key={houseData.id}>
-        <p>
-          {array.length} / {index + 1}
-        </p>
-        <img src={houseData.image} />
-        <div>
-          <ul>
-            <li>
-              <span>Fiyat:</span>
-              {houseData.price}
-            </li>
-            <li>
-              <span>Lokasyon:</span>
-              {houseData.location}
-            </li>
-            <li>
-              <span>Metrekare:</span>
-              {houseData.squareFeet}
-            </li>
-            <li>
-              <span>Dönüm:</span>
-              {houseData.acres}
-            </li>
-            <li>
-              <span>Yapım Yılı:</span>
-              {houseData.yearBuilt}
-            </li>
-            <li>
-              <span>Yatak Odası:</span>
-              {houseData.bedrooms}
-            </li>
-            <li>
-              <span>Banyo:</span>
-              {houseData.bathrooms}
-            </li>
-            <li>
-              <span>Diğer Odalar:</span>
-              {houseData.otherRooms}
-            </li>
-            <li>
-              <span>Garaj:</span>
-              {houseData.garage ? "Evet" : "Hayır"}
-            </li>
-            <li>
-              <span>Klima:</span>
-              {houseData.airConditioning ? "Evet" : "Hayır"}
-            </li>
-            <li>
-              <span>Isıtma Sistemi:</span>
-              {houseData.heating ? "Evet" : "Hayır"}
-            </li>
-            <li>
-              <span>Lanetli:</span>
-              {houseData.haunted ? "Evet" : "Hayır"}
-            </li>
-          </ul>
-        </div>
-      </div>
+      <HouseCard houseData={houseData} index={index} array={array}/>
     );
   });
 
